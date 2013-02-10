91cd4733ed1dc63fe936bc79a4d4aea5b1437e64
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2013-02-10 18:48:29 +0100

    Initial import

---
+++ dot-vim/indent/c.vim
@@ -0,0 +1,13 @@
+let c_gnu=1
+let c_no_curly_error=1
+let c_space_errors=1
+
+setlocal autoindent
+setlocal autowrite
+setlocal cino=t0,l1,(0,W4
+setlocal expandtab
+setlocal expandtab
+setlocal shiftwidth=4 
+setlocal smartindent
+setlocal syntax=objc
+setlocal tabstop=4
---
+++ install-dot-files.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+set -e
+
+# DotFiles need to be in this directory
+BASE_DIR="DotFiles"
+# Don't die on filenames with spaces in them
+IFS=''
+
+# Create each standalone dotfile (dot-FILE as ~/.FILE)
+for F in $HOME/$BASE_DIR/dot-*; do
+    # Skip Ubuntu One synchronization conflicts
+    if echo "$F" | grep -e '.u1conflict' >/dev/null; then
+        continue
+    fi
+    # Compute the real filename
+    F="$(basename "$F" | sed 's/dot-//')"
+    # Set aside local file with the same name _unless_ it is a symlink we want to create already
+    if [ -e "$HOME/.$F" ] && [ ! -h "$HOME/.$F" -o "$(readlink "$HOME/.$F")" != "$HOME/$BASE_DIR/dot-$F" ]; then
+        mv -v "$HOME/.$F" "$HOME/.$F.local"
+    fi
+    # Create a symlink to the dotfile
+    if [ ! -e "$HOME/.$F" ]; then
+        ln -v -s "$HOME/$BASE_DIR/dot-$F" "$HOME/.$F"
+    fi
+done
+
+# Create each dot-directory (as above but for directories)
+for D in $HOME/$BASE_DIR/dirdot-*; do
+    # Skip Ubuntu One synchronization conflicts
+    if echo "$D" | grep -e '.u1conflict' >/dev/null; then
+        continue
+    fi
+    # Compute the real directory name
+    D="$(basename "$D" | sed 's/dirdot-//')"
+    # Create the dot directory if needed
+    if [ ! -d "$HOME/.$D" ]; then
+        mkdir -v "$HOME/.$D"
+    fi
+    # Symlink each dotfile from the dotdirectory
+    for F in $HOME/$BASE_DIR/dirdot-$D/*; do
+        F="$(basename "$F")"
+        if [ -e "$HOME/.$D/$F" ] && [ ! -h "$HOME/.$D/$F" -o "$(readlink "$HOME/.$D/$F")" != "$HOME/$BASE_DIR/dirdot-$D/$F" ] ; then
+        mv -v "$HOME/.$D/$F" "$HOME/.$D/$F.local"
+        fi
+        if [ ! -e "$HOME/.$D/$F" ]; then
+            ln -v -s "$HOME/$BASE_DIR/dirdot-$D/$F" "$HOME/.$D/$F"
+        fi
+    done
+done
